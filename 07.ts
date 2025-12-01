7f882887859ba57d0814947898c862f2466d744f
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-02 02:20:25 +0500

    Базовые типы: строка, число, логическое значение

---
+++ 07.ts
@@ -0,0 +1,12 @@
+// let userName: string = 'Ivan'
+// userName = 5
+
+
+
+const isBirthday: boolean = true;
+const age: number = 40
+const userName: string = 'John'
+
+if(isBirthday){
+    console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`)
+}
