d5f3d07284778074e7c1ffa105eef30568f8004b
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-03 16:11:38 +0500

    Использование системы типов в функциях

---
+++ 08.ts
@@ -0,0 +1,21 @@
+const isBirthdayData: boolean = true
+let ageData: number = 40
+const userNameData: string = 'John'
+
+// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+//     if (isBirthday) {
+//         return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`
+//     } else {
+//         return 'Error'
+//     }
+// }
+
+const logBrtMsg = (isBirthday: boolean, userName: string, age: number): string => {
+    if (isBirthday) {
+        return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`
+    } else {
+        return 'Error'
+    }
+}
+
+console.log(logBrtMsg(isBirthdayData, userNameData, ageData))
