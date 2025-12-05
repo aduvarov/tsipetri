7b3228ae63cf9fa8ade6ccbc6e088508f3a0ea1a
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-05 22:18:05 +0500

    Сужение типов (Narrowing)

--- index.ts
+++ index.ts
@@ -1,9 +1,52 @@
 const message: string | number = 5
 const messages: string[] | number[] = ['a', 'b']
 
-function printMsg(msg: string | number): void {
-    console.log(msg)
+// function printMsg(msg: string | number | boolean): void {
+//     if (typeof msg === 'string' || typeof msg === 'number') {
+//         console.log(msg.toString())
+//     } else {
+//         console.log(msg.valueOf)
+//     }
+
+//     console.log(msg) // union
+// }
+
+function printMsg(msg: string[] | number | boolean): void {
+    if (Array.isArray(msg)) {
+        msg.forEach(m => console.log(m))
+    } else if (typeof msg === 'number') {
+        console.log(msg.toFixed())
+    } else {
+        console.log(msg)
+    }
+
+    console.log(msg) // union
 }
 
 printMsg(4)
-printMsg('abcdef')
+
+const printReadings = (a: number | string, b: number | boolean) => {
+    if (a === b) {
+        console.log(a)
+    }
+}
+
+const printReadings2 = (a: number[] | string) => {
+    console.log(a.slice(0, 3))
+}
+
+function checkReadings(readings: { system: number } | { user: number }): void {
+    if ('system' in readings) {
+        console.log(readings.system)
+    } else {
+        console.log(readings.user)
+    }
+}
+
+function logValue(x: string | Date) {
+    if (x instanceof Date) {
+        console.log(x.getDate())
+    } else {
+        console.log(x.trim())
+    }
+}
