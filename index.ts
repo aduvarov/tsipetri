b1aa59649015fa6015a8933b452f3f2743cfb009
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-06 15:38:08 +0500

    Более продвинутый Type и пересечение типов (Intersection)

--- index.ts
+++ index.ts
@@ -2,12 +2,28 @@ let msg: 'Hello' = 'Hello'
 
 msg = 'Hello'
 
-const serverConfig: { protocol: 'http' | 'https'; port: 3000 | 3001 } = {
-    protocol: 'https',
-    port: 3001,
+type Config = { protocol: 'http' | 'https'; port: 3000 | 3001 }
+type Role = {
+    role: string
 }
 
-const startServer: (protocol: 'http' | 'https', port: 3000 | 3001) => string = (
+type ConfigWithRole = Config & Role
+
+const serverConfig: ConfigWithRole = {
+    protocol: 'https',
+    port: 3001,
+    role: 'admin',
+}
+
+const backupConfig: ConfigWithRole = {
+    protocol: 'http',
+    port: 3000,
+    role: 'sysadmin',
+}
+
+type StartFunction = (protocol: 'http' | 'https', port: 3000 | 3001) => string
+
+const startServer: StartFunction = (
     protocol: 'http' | 'https',
     port: 3000 | 3001
 ): 'Server started' => {
