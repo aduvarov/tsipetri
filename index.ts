1e6b85540b280b4a221bbd4d95cb03fd80c16d7f
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-08 17:51:24 +0500

    Механизм вывода типов (Type Inference)

--- index.ts
+++ index.ts
@@ -1,81 +1,19 @@
-let msg: 'Hello' = 'Hello'
+let salary: number
+salary = 500
 
-msg = 'Hello'
-
-// type Config = { protocol: 'http' | 'https'; port: 3000 | 3001 }
-interface Config {
-    protocol: 'http' | 'https'
-    port: 3000 | 3001
-    log: (msg: string) => void
-}
-// type Role = {
-//     role: string
-// }
-// type ConfigWithRole = Config & Role
-
-interface Role {
-    role: string
+interface UserData {
+    isBirthdayData: boolean
+    ageData: number
+    userNameData: string
 }
 
-interface ConfigWithRole extends Role, Config {}
+const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}'
+const userObj: UserData = JSON.parse(userData)
+console.log(userObj.smt)
 
-const serverConfig: ConfigWithRole = {
-    protocol: 'https',
-    port: 3001,
-    role: 'admin',
-    log: (msg: string): void => {
-        console.log(msg)
-    },
-}
+let isOkay = true
+let movement: boolean | string = false
 
-// const backupConfig: ConfigWithRole = {
-//     protocol: 'http',
-//     port: 3000,
-//     role: 'sysadmin',
-// }
-
-type StartFunction = (
-    protocol: 'http' | 'https',
-    port: 3000 | 3001,
-    log: (msg: string) => void
-) => string
-
-const startServer: StartFunction = (
-    protocol: 'http' | 'https',
-    port: 3000 | 3001,
-    log: (msg: string) => void
-): 'Server started' => {
-    log(`Server started on ${protocol}://server:${port}`)
-    return 'Server started'
-}
-
-startServer(serverConfig.protocol, serverConfig.port, serverConfig.log)
-
-// type AntimationTimingFunction = 'ease' | 'ease-out' | 'ease-in'
-// type AnimationID = string | number
-
-// function createAnimation(
-//     id: AnimationID,
-//     animationName: string,
-//     timingFunc: AntimationTimingFunction = 'ease',
-//     duration: number,
-//     iterCount: 'infinite' | number
-// ): void {
-//     const elem = document.querySelector(`#${id}`) as HTMLElement
-//     if (elem) {
-//         elem.style.animation = `${animationName} ${timingFunc} ${duration} ${iterCount}`
-//         console.log(`${animationName} ${timingFunc} ${duration} ${iterCount}`)
-//     }
-// }
-
-// createAnimation('id', 'fade', 'ease-in', 5, 'infinite')
-
-interface Styles {
-    [key: string]: string
-}
-
-const styles: Styles = {
-    position: 'absolute',
-    top: '20px',
-    left: '50px',
+if (isOkay) {
+    movement = 'moving'
 }
