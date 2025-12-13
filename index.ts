11cd79c7e82c763bd3f3c70613ec712806658ba9
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-13 19:10:05 +0500

    Тип Unknown

--- index.ts
+++ index.ts
@@ -1,29 +1,51 @@
-const TOP = 'Top'
-const RIGHT = 'Right'
+// let smth: unknown
 
-enum Directions {
-    TOP,
-    RIGHT,
-    LEFT,
-    BOTTOM,
-}
+// smth = 'str'
 
-const enum TimingFunc {
-    EASE = 'ease',
-    EASE_IN = 'ease-in',
-    LINEAR = 'linear',
-}
+// let data: string[] = smth
+// data.find(e => e)
 
-const enum TimingFuncT {
-    EASE = 1,
-    EASE_IN = 2,
-    LINEAR = EASE * 2,
-}
+// const someValue: unknown = 10
+// someValue.method()
 
-function frame(elem: string, dir: Directions, tFunc: TimingFunc): void {
-    if (dir === Directions.RIGHT) {
-        console.log('tFunc: ', tFunc)
+function fetchData(data: unknown): void {
+    if (typeof data === 'string') {
+        console.log(data.toLowerCase())
     }
 }
 
-frame('id', Directions.RIGHT, TimingFunc.EASE)
+const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}'
+
+function safeParse(s: string): unknown {
+    return JSON.parse(s)
+}
+
+const data = safeParse(userData)
+
+function transferData(d: unknown): void {
+    if (typeof d === 'string') {
+        console.log(d.toLowerCase())
+    } else if (typeof d === 'object' && d) {
+        console.log(d)
+    } else {
+        console.error('Some error')
+    }
+}
+
+transferData(data)
+
+try {
+    if (1) throw new Error('error')
+} catch (e) {
+    if (e instanceof Error) {
+        console.log(e.message)
+    } else if (typeof e === 'string') {
+        console.log(e)
+    }
+}
+
+type T0 = any | unknown
+type T1 = number | unknown
+
+type T3 = any & unknown
+type T4 = number & unknown
