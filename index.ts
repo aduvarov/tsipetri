d67ebe3f8ec4e8c8a61127e2002768529969bfbb
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-13 20:34:33 +0500

    Запросы типов

--- index.ts
+++ index.ts
@@ -1,51 +1,20 @@
-// let smth: unknown
+const dataFromControl = {
+    water: 200,
+    el: 350,
+}
 
-// smth = 'str'
-
-// let data: string[] = smth
-// data.find(e => e)
-
-// const someValue: unknown = 10
-// someValue.method()
-
-function fetchData(data: unknown): void {
-    if (typeof data === 'string') {
-        console.log(data.toLowerCase())
+function checkReadings(data: typeof dataFromControl): boolean {
+    const dataFromUser = {
+        water: 200,
+        el: 350,
     }
-}
 
-const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}'
-
-function safeParse(s: string): unknown {
-    return JSON.parse(s)
-}
-
-const data = safeParse(userData)
-
-function transferData(d: unknown): void {
-    if (typeof d === 'string') {
-        console.log(d.toLowerCase())
-    } else if (typeof d === 'object' && d) {
-        console.log(d)
+    if (data.el === dataFromUser.el && data.water === dataFromUser.water) {
+        return true
     } else {
-        console.error('Some error')
+        return false
     }
 }
 
-transferData(data)
-
-try {
-    if (1) throw new Error('error')
-} catch (e) {
-    if (e instanceof Error) {
-        console.log(e.message)
-    } else if (typeof e === 'string') {
-        console.log(e)
-    }
-}
-
-type T0 = any | unknown
-type T1 = number | unknown
-
-type T3 = any & unknown
-type T4 = number & unknown
+const PI = 3.14
+let PIClone: typeof PI
