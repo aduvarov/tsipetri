b5ed2df2d65a4ba258b1caada72714057431037e
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-13 21:12:21 +0500

    Практика

--- index.ts
+++ index.ts
@@ -1,20 +1,68 @@
-const dataFromControl = {
-    water: 200,
-    el: 350,
+// Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
+enum TypesOfMedia {
+    VIDEO = 'video',
+    AUDIO = 'audio',
 }
 
-function checkReadings(data: typeof dataFromControl): boolean {
-    const dataFromUser = {
-        water: 200,
-        el: 350,
-    }
+// Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
+enum FormatsOfMedia {
+    MP4 = '.mp4',
+    MOV = '.mov',
+    MKV = '.mkv',
+    FLV = '.flv',
+    WEBM = '.webM',
+}
 
-    if (data.el === dataFromUser.el && data.water === dataFromUser.water) {
-        return true
+// Описание интерфейса, в котором:
+// name - строка
+// type - один из перечисления выше
+// format = один из перечисления выше
+// subtitles - необязательное поле типа строка
+// marks - необязательное поле неизвестного типа
+
+interface IMedia {
+    name: string
+    type: TypesOfMedia
+    format: FormatsOfMedia
+    subtitles?: string
+    marks?: unknown
+}
+
+function playMedia(
+    { name, type, format, subtitles, marks }: IMedia = {
+        name: 'example',
+        type: TypesOfMedia.VIDEO,
+        format: FormatsOfMedia.MP4,
+    }
+): string {
+    let marksLog: string
+
+    if (marks) {
+        if (Array.isArray(marks)) {
+            marksLog = marks.join(' ')
+        } else if (typeof marks === 'string') {
+            marksLog = marks
+        }
     } else {
-        return false
+        marksLog = 'Unsupported type of marks'
     }
+    // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+    // Если это строка, то просто поместить её в marksLog
+    // Если что-то другое - то marksLog = "Unsupported type of marks"
+    // Не допускайте any!
+
+    console.log(`Media ${name}${format} is ${type}
+    Marks: ${marksLog!}
+    Subtitles: ${subtitles ?? 'none'}`)
+    // помните что это за оператор ??
+
+    return 'Media started'
 }
 
-const PI = 3.14
-let PIClone: typeof PI
+playMedia({
+    name: 'WoW',
+    format: FormatsOfMedia.MOV,
+    type: TypesOfMedia.VIDEO,
+    subtitles: 'hmhmhm hmhmhm doh',
+    marks: ['4:30', '5:40'],
+})
