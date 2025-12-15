61b531af6ce0d8f776f38982edb89182fbca5c49
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-16 03:08:35 +0500

    Практика создания type guard и правильных интерфейсов

--- index.ts
+++ index.ts
@@ -1,63 +1,89 @@
-interface Car {
-    name: 'car'
-    engine: string
-    wheels: {
-        number: number
-        type: string
+// Request
+// {
+//     animal: 'cat' | 'dog' | 'bird',
+//     breed: string,
+//     sterilized?: string
+// }
+
+// Response #1
+
+// {
+//     status: 'available',
+//     data: {
+//         animal: 'cat' | 'dog' | 'bird',
+//         breed: string,
+//         sterilized?: string,
+//         location: string,
+//         age?: number
+//     }
+// }
+
+// Response #2
+
+// {
+//     status: 'not available',
+//     data: {
+//         message: string,
+//         nextUpdateIn: Date
+//     }
+// }
+
+interface IAnimal {
+    animal: 'cat' | 'dog' | 'bird'
+    breed: string
+    sterilized?: string
+}
+
+interface ISucessData extends IAnimal {
+    location: string
+    age?: number
+}
+
+interface IFailData {
+    message: string
+    nextUpdateIn: Date
+}
+interface ISuccessResponse {
+    status: 'available'
+    data: ISucessData
+}
+
+interface IFailResponse {
+    status: 'not available'
+    data: IFailData
+}
+
+function isAvailable(response: ISuccessResponse | IFailResponse): response is ISuccessResponse {
+    return 'location' in response.data
+}
+
+const r1: ISuccessResponse = {
+    status: 'available',
+    data: {
+        animal: 'cat',
+        breed: 'britan',
+        sterilized: 'yes',
+        location: 'n8',
+        age: 3,
+    },
+}
+
+const r2: IFailResponse = {
+    status: 'not available',
+    data: {
+        message: 'Not found',
+        nextUpdateIn: new Date(),
+    },
+}
+
+function checkAnimalData(animal: ISuccessResponse | IFailResponse) {
+    if (isAvailable(animal)) {
+        // Заменить условие!
+        return animal.data
+    } else {
+        return `${animal.data.message}, you can try in ${animal.data.nextUpdateIn}`
     }
 }
 
-interface Ship {
-    name: 'ship'
-    engine: string
-    sail: string
-}
-
-interface Airplane {
-    name: 'airplane'
-    engine: string
-    wings: string
-}
-
-interface ComplexVehicle {
-    name: 'car' | 'ship' | 'airplane'
-    engine: string
-    wheels?: number
-    sail?: string
-    wings?: string
-}
-
-type Vehicle = Car | Ship | Airplane
-
-function isCar(car: Vehicle): car is Car {
-    // return 'wheels' in car
-    return (car as Car).wheels.number !== undefined
-}
-
-function isShip(ship: Vehicle): ship is Ship {
-    return 'sail' in ship
-}
-
-const car: ComplexVehicle = {
-    name: 'car',
-    engine: 'V8',
-}
-
-function repairVehicle(vehicle: ComplexVehicle) {
-    switch (vehicle.name) {
-        case 'car':
-            console.log(vehicle.wheels! * 2)
-            break
-        case 'ship':
-            console.log(vehicle.sail)
-            break
-        case 'airplane':
-            console.log(vehicle.wings)
-            break
-        default:
-            // const smth: never = vehicle
-            console.log('Ouuuups!')
-    }
-}
-
-repairVehicle(car)
+console.log(checkAnimalData(r1))
+console.log(checkAnimalData(r2))
