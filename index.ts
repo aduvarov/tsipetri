32f485aecb60908afdf9df44740ed11193986458
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-15 22:05:12 +0500

    Кейс использования never

--- index.ts
+++ index.ts
@@ -21,6 +21,7 @@ function isNumber(n: unknown): n is number {
 }
 
 interface Car {
+    name: 'car'
     engine: string
     wheels: {
         number: number
@@ -29,25 +30,54 @@ interface Car {
 }
 
 interface Ship {
+    name: 'ship'
     engine: string
     sail: string
 }
 
-function repairVehicle(vehicle: Car | Ship) {
-    if (isCar(vehicle)) {
-        console.log(vehicle.wheels)
-    } else if (isShip(vehicle)) {
-        console.log(vehicle.sail)
-    } else {
-        vehicle // never
-    }
+interface Airplane {
+    name: 'airplane'
+    engine: string
+    wings: string
 }
 
-function isCar(car: Car | Ship): car is Car {
+interface SuperAirplane {
+    name: 'smth'
+    engine: string
+    wings: string
+}
+
+type Vehicle = Car | Ship | Airplane | SuperAirplane
+
+function isCar(car: Vehicle): car is Car {
     // return 'wheels' in car
     return (car as Car).wheels.number !== undefined
 }
 
-function isShip(ship: Car | Ship): ship is Ship {
+function isShip(ship: Vehicle): ship is Ship {
     return 'sail' in ship
+}
+
+function repairVehicle(vehicle: Vehicle) {
+    // if (isCar(vehicle)) {
+    //     console.log(vehicle.wheels)
+    // } else if (isShip(vehicle)) {
+    //     console.log(vehicle.sail)
+    // } else {
+    //     vehicle.wings
+    // }
+    switch (vehicle.name) {
+        case 'car':
+            console.log(vehicle.wheels)
+            break
+        case 'ship':
+            console.log(vehicle.sail)
+            break
+        case 'airplane':
+            console.log(vehicle.wings)
+            break
+        default:
+            const smth: never = vehicle
+            console.log('Ouuuups!')
+    }
 }
