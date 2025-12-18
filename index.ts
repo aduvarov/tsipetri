cb16207b6f76a0fa3f2561ea92cff5effbf92b76
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-18 22:58:46 +0500

    Generics types and interfaces, constraints

--- index.ts
+++ index.ts
@@ -1,40 +1,53 @@
-function processingData<T, S>(data: T[], options: S): string {
-    data.length
-    switch (typeof data) {
-        case 'string':
-            return `${data}, speed: ${options}`
-            break
-        case 'number':
-            return `${data}, speed: ${options}`
-            break
-        default:
-            return 'Not valid'
-    }
+interface ProcessingFn {
+    <T>(data: T): T
 }
-
-const res1 = processingData([1], 'fast')
-let res2 = processingData(['1'], 'slow')
-
-const num = 100
-const res3 = processingData<number, string>([num], 'slow')
 
 function processing<T>(data: T): T {
     return data
 }
 
-interface ProcessingFn {
-    <T>(data: T): T
-}
-
 let newFunc: ProcessingFn = processing
-interface DataSaver {
-    processing: typeof processing
+
+type Smth<T> = T
+
+const num: Smth<number> = 5
+
+interface ParentsOfUser {
+    mother: string
+    father: string
 }
 
-const saver: DataSaver = {
-    // processing(data) {
-    //     console.log(data)
-    //     return data;
-    // },
-    processing: processing,
+type User<ParentsData extends ParentsOfUser> = {
+    login: string
+    age: number
+    parents: ParentsData
 }
+
+const user: User<{ mother: string; father: string; married: boolean }> = {
+    login: 'str',
+    age: 54,
+    parents: { mother: 'Anna', father: 'no data', married: true },
+}
+
+type OrNull<Type> = Type | null
+type OneOrMany<Type> = Type | Type[]
+
+const data: OneOrMany<number[]> = [5]
+
+// const depositMoney = <T extends number | string>(amount: T): T => {
+//     console.log(`req to server with amount: ${amount}`)
+//     return amount
+// }
+
+// depositMoney(500)
+// depositMoney('500')
+// // depositMoney(false)
+
+const depositMoney = (amount: number | string): number | string => {
+    console.log(`req to server with amount: ${amount}`)
+    return amount
+}
+
+depositMoney(500)
+depositMoney('500')
+depositMoney(false)
