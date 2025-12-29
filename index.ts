52b360a3aa53b766af5287c155587a51229fd9c8
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-30 00:53:27 +0500

    Indexed Access Types

--- index.ts
+++ index.ts
@@ -1,7 +1,28 @@
+import { isClassOrTypeElement } from 'typescript'
+
 interface ICompany {
     name: string
     debts: number
+    departments: Department[]
+    management: {
+        owner: string
+    }
 }
+
+interface Department {
+    [key: string]: string
+}
+
+// let debts = 'debts' as 'debts'
+let debts: 'debts' = 'debts'
+type CompanyDebtsType = ICompany[typeof debts]
+
+// type CompanyDebtsType = ICompany['debts']
+type CompanyOwnerType = ICompany['management']['owner']
+type CompanyDepartmentsType = ICompany['departments'][number]
+type CompanyDepartmentsTypes = ICompany['departments']
+
+type Test = ICompany[keyof ICompany]
 
 type CompanyKeys = keyof ICompany
 const keys: CompanyKeys = 'debts' // 'debts' или 'name'
@@ -13,6 +34,13 @@ function printDebts<T, K extends keyof T, S extends keyof T>(company: T, name: K
 const google: ICompany = {
     name: 'Google',
     debts: 500000,
+    departments: {
+        sales: 'sales',
+        developer: 'dev',
+    },
+    management: {
+        owner: 'John Doe',
+    },
 }
 
 printDebts(google, 'name', 'debts') // аргументы №2 и №3 строго ограничены свойствами
