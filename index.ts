fea84c0809d1c8e63a1fc76b7504882287f07561
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-01-08 16:12:01 +0500

    Конструкторы, перегрузки и дженерики

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 class Box {
     width: number
     height: number
+    volume: string
 
-    constructor(width: number) {
-        this.width = width
+    constructor(volume: string)
+    constructor(width: number)
+    constructor(widthOrVolume: number | string) {
+        if (typeof widthOrVolume === 'number') {
+            this.width = widthOrVolume
+        } else {
+            this.volume = widthOrVolume
+        }
         this.height = 500
     }
+    // constructor(width: number) {
+    //     this.width = widthOrVolume
+    //     this.height = 500
+    // }
 }
 
 const firstBox = new Box(250)
