2b8f538e6fb6c3e9b668030e719d6bfbf9c3d54f
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2026-01-11 13:37:51 +0500

    Модификаторы видимости свойств

--- index.ts
+++ index.ts
@@ -1,70 +1,42 @@
-enum TransferStatus {
-    Pending = 'pending',
-    Rejected = 'rejected',
-    Completed = 'completed',
-}
+class Player {
+    private login: string
+    private _password: string
+    public server: string
+    protected consent: boolean
 
-enum ErrorMessages {
-    NotFound = 'Not found: 404',
-    NotEnoughSpace = 'Not enough space: 507',
-    Forbidden = 'Forbidden: 403',
-}
-
-interface ITransfer {
-    path: string
-    data: string[]
-    date?: Date
-    start: (p: string, d: string[]) => string
-    stop: (reason: string) => string
-}
-
-interface TransferError {
-    message: ErrorMessages
-}
-
-// Класс должен имплементировать ITransfer и TransferError
-class SingleFileTransfer implements ITransfer, TransferError {
-    // Место для реализаций
-
-    path: string
-    data: string[]
-    date?: Date | undefined
-    transferStatus: TransferStatus
-    message: ErrorMessages
-
-    start = (p: string, d: string[]) => {
-        this.path = p
-        this.data = d
-        this.transferStatus = TransferStatus.Pending
-        return 'started'
+    get password() {
+        return this._password
     }
 
-    // Необходимо создать метод checkTransferStatus, проверяющий состояние передачи данных
-    // Можно вывести в консоль данные, можно вернуть строку
-    checkTransferStatus() {
-        return this.transferStatus
-    }
-
-    // Необходимо создать метод, который будет останавливать передачу данных
-    // И возвращать строку с причиной и датой остановки (Дата в любом формате)
-    stop = (reason: string) => {
-        this.transferStatus = TransferStatus.Rejected
-        return `${reason} ${new Date()}`
-    }
-
-    // Необходимо создать метод, который будет возвращать строку, содержащую
-    // Статус передачи и любое сообщение об ошибке. На ваш выбор или отталкиваться от приходящего аргумента
-    // Метод может показаться странным, но может использоваться для тестов, например
-
-    getTransferStatus(): string {
-        return `${this.transferStatus} ${ErrorMessages.NotFound}`
+    set password(newPass: string) {
+        // Validation
+        this._password = newPass
     }
 }
 
-const sft = new SingleFileTransfer()
+class CompetitivePlayer extends Player {
+    rank: number
 
-console.log(sft.start('firstString', ['one', 'two']))
-console.log(sft.checkTransferStatus())
-console.log(sft.stop('network error'))
-console.log(sft.checkTransferStatus())
-console.log(sft.getTransferStatus())
+    isConsented() {
+        return this.consent ? 'Yes' : 'No'
+    }
+}
+
+// const player = new Player()
+const player = new CompetitivePlayer()
+player.password = '1qaz'
+// player.login = 'abcd'
+
+// class User {
+//     public email: string
+//     public name: string
+
+//     constructor(email: string, name: string){
+//         this.email = email
+//         this.name = name
+//     }
+// }
+
+// class User {
+//     constructor(public email: string, public name: string){}
+// }
